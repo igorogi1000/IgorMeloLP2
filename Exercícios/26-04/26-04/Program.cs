63fe6277ad95d4df4cfa48ba6759e95f7c37e863
Igor Melo <igoracm@outlook.com>
2017-05-03 13:23:32 -0300

    1ª etapa do exercício

--- Exercícios/26-04/26-04/Program.cs
+++ Exercícios/26-04/26-04/Program.cs
@@ -1,59 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace exercicios_lp2
 {
     class Program
     {
-        //Classificar("celta", 123, 120);
-        static string Classificar(string nome, double kmRodados, int potenciaMotor)
-        {
-            string idade;
-            string potenciaPorExtenso;
-
-            //Idade do carro
-            if (kmRodados <= 5000)
-                idade = "novo";
-
-            else if (kmRodados > 5000 && kmRodados <= 30000)
-                idade = "seminovo";
-
-            else
-                idade = "velho";
-
-            //Potencia 
-            if (potenciaMotor >= 120 && potenciaMotor <= 200)
-                potenciaPorExtenso = "forte";
-
-            else if (potenciaMotor > 200)
-                potenciaPorExtenso = "potente";
-
-            else
-                potenciaPorExtenso = "popular";
-
-            return nome + " " + idade + " " + potenciaPorExtenso;
-        }
-
         static void Main(string[] args)
         {
-            
             int quantidadeVeiculos;
-            
+            string nome;
+            double kmRodados;
+            int potenciaMotor;
+
+
             Console.WriteLine("Digite a quantidade de veículos: ");
             quantidadeVeiculos = int.Parse(Console.ReadLine());
 
-            for (int i=0; i < quantidadeVeiculos; i++)
+            for (int i = 0; i < quantidadeVeiculos; i++)
             {
-                string modelo;
-                double kmRodados;
-                int potenciaMotor;
 
                 //Recebendo valores
+
                 Console.WriteLine("Digite o modelo: ");
-                modelo = Console.ReadLine();
+                nome = Console.ReadLine();
                 Console.WriteLine();
 
                 Console.WriteLine("Digite a quilometragem rodada: ");
@@ -64,11 +32,31 @@ namespace ConsoleApplication1
                 potenciaMotor = int.Parse(Console.ReadLine());
                 Console.WriteLine("----------------");
 
+                string idade;
+                string potenciaPorExtenso;
 
-                string resultado = Classificar(modelo, kmRodados, potenciaMotor);
-                Console.WriteLine(resultado);
+                //Idade do carro
+                if (kmRodados <= 5000)
+                    idade = "novo";
+
+                else if (kmRodados > 5000 && kmRodados <= 30000)
+                    idade = "seminovo";
+
+                else
+                    idade = "velho";
+
+                //Potencia 
+                if (potenciaMotor >= 120 && potenciaMotor <= 200)
+                    potenciaPorExtenso = "forte";
+
+                else if (potenciaMotor > 200)
+                    potenciaPorExtenso = "potente";
+
+                else
+                    potenciaPorExtenso = "popular";
+
+                Console.WriteLine("{0} - {1} {2}", nome, idade, potenciaPorExtenso);
             }
-
         }
     }
 }
