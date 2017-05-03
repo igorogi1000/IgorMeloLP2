f9481d680ae107fee636a64293d8fef676891041
Igor Melo <igoracm@outlook.com>
2017-05-03 15:53:55 -0300

    Exercício de vogais

---
+++ Exercícios/03-05/Vogais/Vogais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine().ToLower();
+            char[] vogais = { 'a', 'e', 'i', 'o', 'u' };
+
+            int qtdVogais = 0;
+
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (vogais.Contains(frase[i]))
+                    qtdVogais++;
+            }
+
+            Console.WriteLine("Contém {0} vogais na frase", qtdVogais);
+        }
+    }
+}
