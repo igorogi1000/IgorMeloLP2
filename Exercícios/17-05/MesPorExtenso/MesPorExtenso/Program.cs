505382100205265c44bf6049b852cff81b4e2aa6
Igor <igoracm@outlook.com>
2017-05-17 15:41:48 -0300

    Mês por extenso

---
+++ Exercícios/17-05/MesPorExtenso/MesPorExtenso/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesPorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ata = mesPorExtenso("17/05/2017");
+            Console.WriteLine(ata);
+        }
+
+        static string mesPorExtenso(string data)
+        {
+            string[] meses = {"janeiro", "feveiro", "março", "abril", "maio", "junho", 
+                             "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+
+            string dia = data.Substring(0, 2);
+            int mes = int.Parse(data.Substring(3, 2));
+            string ano = data.Substring(6, 4);
+
+            return String.Format("{0} de {1} de {2}", dia, meses[mes-1], ano);
+        }
+    }
+}
