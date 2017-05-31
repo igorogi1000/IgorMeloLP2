95e4d777be1d3523e7a865cf8229a53f99839c4c
Igor Melo <igoracm@outlook.com>
2017-05-31 16:02:55 -0300

    . Movimento Objeto2D

---
+++ Exercícios/31-05/ObjetosNoPlano/ObjetosNoPlano/Objeto2D.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Objeto2D
+    {
+        public int x;
+        public int y;
+
+        public const bool POSITIVO = true;
+        public const bool NEGATIVO = false;
+        public const int PASSO = 3;
+
+        public void moveX(bool direita)
+        {
+            if (direita)
+                x += PASSO;
+            else
+                x -= PASSO;
+        }
+
+        public void moveY(bool cima)
+        {
+            if (cima)
+                y += PASSO;
+            else
+                y -= PASSO;
+        }
+
+        public string Coordenadas()
+        {
+            return String.Format("[{0}, {1}]", x, y);
+        }
+
+        /*public string CordenadasString(int w, int h)
+        {
+            char[,] resultado = new char[h, w];
+            for (int l = 0; l < w; l++)
+            {
+                for (int c = 0; c < h; c++) 
+                {
+                    if (l == x && c == y)
+                        resultado[l,c] = '!';
+                    else 
+                        resultado[l,c] = '.';
+                }
+                    
+            }
+            return resultado.ToString();
+        }*/
+    }
+}
---
+++ Exercícios/31-05/ObjetosNoPlano/ObjetosNoPlano/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int W = 800;
+            int H = 600;
+
+            bool continuar = true;
+
+            Objeto2D o = new Objeto2D();
+
+            Console.WriteLine("Digite x inicial: ");
+            o.x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite y inicial: ");
+            o.y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pressione o comando");
+
+            while (continuar) {
+                
+                ConsoleKey comando = Console.ReadKey().Key;
+                Console.WriteLine();
+
+                if (comando == ConsoleKey.D && o.x + Objeto2D.PASSO <= W)
+                    o.moveX(Objeto2D.POSITIVO);
+
+                else if (comando == ConsoleKey.A && o.x - Objeto2D.PASSO >= 0)
+                    o.moveX(Objeto2D.NEGATIVO);
+
+                else if (comando == ConsoleKey.W && o.y + Objeto2D.PASSO <= H)
+                    o.moveY(Objeto2D.POSITIVO);
+
+                else if (comando == ConsoleKey.S && o.y - Objeto2D.PASSO >= 0)
+                    o.moveY(Objeto2D.NEGATIVO);
+
+                else if (comando == ConsoleKey.Escape)
+                    continuar = false;
+
+                Console.WriteLine("[{0}, {1}]", o.x, o.y);
+            }
+
+            Console.WriteLine("Nice-ended");
+        }
+    }
+}
