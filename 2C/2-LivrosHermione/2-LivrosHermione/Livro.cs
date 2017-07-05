c8fc47dd6137597302e6628b8c8ac959bdee90b3
Igor <igoracm@outlook.com>
2017-07-05 15:55:50 -0300

    Incompleto - Livros Hermione (entendi errado e fiz bem errado)

---
+++ 2C/2-LivrosHermione/2-LivrosHermione/Livro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosHermione
+{
+    class Livro
+    {
+        public string titulo;
+        public int numeroPaginas;
+        public int diasParaDevolucao;
+
+        public int TempoDeLeitura(int pagPorHora, int horasPorDia)
+        {
+            return pagPorHora * horasPorDia;               
+        }
+    }
+}
---
+++ 2C/2-LivrosHermione/2-LivrosHermione/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosHermione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de livros: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Livro[] livros = new Livro[n];
+
+            Livro l = new Livro();
+
+            //l.TempoDeLeitura(12);
+
+            string[] impossiveis = new string[n];
+            int qtdImpossiveis = 0;
+
+            double horasParaLerTudo = 0;
+
+            Console.WriteLine("Digite a velocidade de leitura de Hermione: ");
+            int velocidade = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                livros[i] = new Livro();
+                
+                Console.WriteLine("Digite o nome do livro: ");
+                livros[i].titulo = Console.ReadLine();
+
+                Console.WriteLine("Digite o número de páginas: ");
+                livros[i].numeroPaginas = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o número de dias para devolução: ");
+                livros[i].diasParaDevolucao = int.Parse(Console.ReadLine());
+
+                Console.Write("\n\n");
+
+                double tempoAtual = livros[i].TempoDeLeitura(velocidade);
+
+                horasParaLerTudo += tempoAtual;
+                if (tempoAtual / 24 > livros[i].diasParaDevolucao) {
+                    impossiveis[qtdImpossiveis] = livros[i].titulo;
+                    qtdImpossiveis++;
+                }
+            }
+
+            if (qtdImpossiveis > 0)
+            {
+                Console.WriteLine("Estes livros são impossíveis de serem lidos com essa lerdeza: ");
+                for (int i = 0; i < qtdImpossiveis-1; i++)
+                {
+                    Console.WriteLine("{0}, ", impossiveis[i]);
+                }
+                Console.WriteLine(impossiveis[qtdImpossiveis - 1]);
+            }
+            
+        }
+    }
+}
