3a589fd6b0fd0039218def261fa4fb839e263232
Igor <igoracm@outlook.com>
2017-08-02 15:52:15 -0300

    03

---
+++ 2C/03-Temperatura/03-Temperatura/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperaturaCelsius t = new TemperaturaCelsius();
+            Console.WriteLine("Digite a temperatura em ºC: ");
+            t.modulo = double.Parse(Console.ReadLine());
+            Console.WriteLine(t.ConverterParaFarenheit());
+        }
+    }
+}
---
+++ 2C/03-Temperatura/03-Temperatura/TemperaturaCelsius.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Temperatura
+{
+    class TemperaturaCelsius
+    {
+        public double modulo;
+        public double ConverterParaFarenheit()
+        {
+            return (double) modulo / 5 * 9 + 32;
+        }
+
+        public double ConverterParaKelvin()
+        {
+            return modulo + 273;
+        }
+    }
+}
+ 
