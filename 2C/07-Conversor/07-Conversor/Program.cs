4a646f92e0078d8b6996e603ad168dc938b5c6bb
Rafaela Gonçalves <rafaelaga46@gmail.com>
2017-09-22 10:59:28 -0300

    Conversor finalizado

--- 2C/07-Conversor/07-Conversor/Program.cs
+++ 2C/07-Conversor/07-Conversor/Program.cs
@@ -12,29 +12,47 @@ namespace _07_Conversor
     {
         static void Main(string[] args)
         {
-            string[] tiposDeConversao = new string[] { "Sair", "Massa", "Temperatura", "Distância" };
-            
-            string[,] conversao = new string[,] {
-                {"Kg --> g", "g --> Kg", "Kg --> T", "T --> Kg", "Kg --> Lbs", "Lbs --> Kg"},
-                {"ºC --> °F", "ºF --> °C", "°F --> °K", "°K --> °F", "°K --> °C", "°C --> °K"},
-                {"Km --> Milha", "Milha --> Km", "m --> pés", "pés --> m", "m --> pol.", "pol. --> m", "pol. --> pés", "pés --> pol."}
-            };
-            
             int selecionou = -1;
 
             while (selecionou != 0)
             {
-                Console.WriteLine("0) {0}", tiposDeConversao[0]);
-                Console.WriteLine("1) {0}", tiposDeConversao[1]);
-                Console.WriteLine("2) {0}", tiposDeConversao[2]);
-                Console.WriteLine("3) {0}", tiposDeConversao[3]);
-
-                selecionou = int.Parse(Console.ReadLine());
+                Console.WriteLine("Escolha uma conversão: ");
+                
+                Console.WriteLine("1) Kg --> g");
+                Console.WriteLine("2) g --> Kg");
+                Console.WriteLine("3) Kg --> T");
+                Console.WriteLine("4) T --> Kg");
+                Console.WriteLine("5) °C --> °F");
+                Console.WriteLine("6) °F --> °C");
 
                 
-            }
+                selecionou = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("É nóis");
+                Console.WriteLine("Digite o valor a ser convertido: ");
+                double valor = double.Parse(Console.ReadLine());
+
+                switch (selecionou)
+                {
+                    case 1:
+                        Console.WriteLine("{0:0.0} g", Conversor.KgParaG(valor));
+                        break;
+                    case 2:
+                        Console.WriteLine("{0:0.0} kg", Conversor.GParaKg(valor));
+                        break;
+                    case 3:
+                        Console.WriteLine("{0:0.0} T", Conversor.KgParaT(valor));
+                        break;
+                    case 4:
+                        Console.WriteLine("{0:0.0} kg", Conversor.TParaKg(valor));
+                        break;
+                    case 5:
+                        Console.WriteLine("{0:0.0} ºF", Conversor.CparaF(valor));
+                        break;
+                    case 6:
+                        Console.WriteLine("{0:0.0} ºC", Conversor.FparaC(valor));
+                        break;
+                }
+            }
         }
     }
 }
